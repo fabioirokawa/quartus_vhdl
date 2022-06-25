56ced0020badd0f279b5ec19c9289ed094a5a3fa
FlashBack <fabio.irokawa1@hotmail.com>
2022-06-24 23:35:49 -0300

    SB e LB

--- control.vhd
+++ control.vhd
@@ -6,9 +6,9 @@ ENTITY control IS
 PORT(
 	clock: IN BIT;
 
-	opcode_in 	: in BIT_VECTOR(6 DOWNTO 0);
-	funct3_in 	: in BIT_VECTOR(2 DOWNTO 0);
-	funct7_in 	: in BIT_VECTOR(6 DOWNTO 0);
+	opcode_out 	: in BIT_VECTOR(6 DOWNTO 0);
+	funct3_out 	: in BIT_VECTOR(2 DOWNTO 0);
+	funct7_out 	: in BIT_VECTOR(6 DOWNTO 0);
 	
 	write_back 	: out bit;
 	alu_src	  	: out bit;
@@ -26,9 +26,9 @@ ARCHITECTURE control_arch OF control IS
 BEGIN
 	PROCESS (clock)
 	BEGIN
-		case opcode_in is
+		case opcode_out is
 			when "1100011" => --BEQ ou BNE
-				if (funct3_in = "000") then --BEQ
+				if (funct3_out = "000") then --BEQ
 					write_back	<=	 	'0';
 					alu_src	  	<=		'0';
 					mem_read	<=			'0';
@@ -38,7 +38,7 @@ BEGIN
 					alu_op	  	<=		"00110";	--XOR
 				end if;
 					
-				if (funct3_in = "001") then  --BNE
+				if (funct3_out = "001") then  --BNE
 					write_back	<=	 	'0';
 					alu_src	  	<=		'0';
 					mem_read	<=			'0';
@@ -64,6 +64,24 @@ BEGIN
 					mem_write	<=		'0';
 					branch		<=		'0';
 					mem_to_reg	<=		'1';
+					alu_op	  	<=		"00000";
+
+			when "0000011" => --LB
+					write_back	<=	 	'1';
+					alu_src	  	<=		'1';
+					mem_read	<=			'1';
+					mem_write	<=		'0';
+					branch		<=		'0';
+					mem_to_reg	<=		'0';
+					alu_op	  	<=		"00000";
+
+			when "0100011" => --SB
+					write_back	<=	 	'0';
+					alu_src	  	<=		'1';
+					mem_read	<=			'0';
+					mem_write	<=		'1';
+					branch		<=		'0';
+					mem_to_reg	<=		'0'; --tanto faz, ta salvando uai
 					alu_op	  	<=		"00000";
 
 			when others => alu_op <= "00000";
